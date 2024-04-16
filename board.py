2636ee684b59d9e4742584a6a172a2624ce6d640
santarsiero <nick.santarsiero@icloud.com>
2024-04-16 15:40:14 -0400

    board 1

---
+++ board.py
@@ -0,0 +1,7 @@
+class Board:
+    def __init__(self, width, height, screen, difficulty):
+        self.width = width
+        self.height = height
+        self.screen = screen
+        self.difficulty = difficulty
+
